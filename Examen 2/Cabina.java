adee003288c4f1b7f62ccbaddf8f55653e3dc028
Osvaldo341 <saraviachipanaosvaldojoaquin@gmail.com>
2025-11-17 10:04:55 -0400

    Refactor Cabina class to use array for passengers
    Refactored Cabina class to use an array for storing passengers instead of ArrayList. Added methods for adding a person, getting the number of persons, and calculating total weight.

--- Examen 2/Cabina.java
+++ Examen 2/Cabina.java
@@ -1,12 +1,30 @@
-package hola;
-import java.util.*;
 public class Cabina {
-	private int nroCabina;
-	ArrayList<Persona> Personaabordo= new ArrayList<Persona>();
-	public Cabina(int nroCabina) {
-		this.nroCabina = nroCabina;
-	}
-	public void AgregarPersona(Persona p) {
-		Personaabordo.add(p);
-	}
+    private int nroCabina;
+    private Persona[] personasAbordo;
+    Cabina(int nroCabina){
+        this.nroCabina=nroCabina;
+        Persona[] personasAbordo = new Persona[50];
+    }
+    public void agregarPersona(Persona p){
+        personasAbordo[obtenerNroPersonas()]=p;
+    }
+    public int obtenerNroPersonas(){
+        return personasAbordo.length;
+    }
+
+    public int getNroCabina() {
+        return nroCabina;
+    }
+
+    public Persona[] getPersonasAbordo() {
+        return personasAbordo;
+    }
+    float ObtenerPeso(){
+        float c=0;
+        for (int i =1; i<personasAbordo.length;i++){
+            c = c+ personasAbordo[i].getPesoPersona();
+        }
+        return c;
+    }
 }
+
