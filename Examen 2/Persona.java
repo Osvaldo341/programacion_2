08a38b4a276d5a89b5741defa8e80e4eb505157d
Osvaldo341 <saraviachipanaosvaldojoaquin@gmail.com>
2025-11-17 10:07:08 -0400

    Update Persona.java

--- Examen 2/Persona.java
+++ Examen 2/Persona.java
@@ -1,14 +1,34 @@
-package hola;
-
 public class Persona {
-	private String nombre;
-	private int edad;
-	private float peso;
-	public Persona(String nom, int ed, float pe) {
-		this.nombre=nom;
-		this.edad=ed;
-		this.peso=pe;
-	}
-	
+    private String nombre;
+    private int edad;
+    private float pesoPersona;
+    Persona(String nombre, int edad, float peso){
+        this.edad=edad;
+        this.nombre=nombre;
+        this.pesoPersona=peso;
+    }
 
+    public float getPesoPersona() {
+        return pesoPersona;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPesoPersona(float pesoPersona) {
+        this.pesoPersona = pesoPersona;
+    }
 }
